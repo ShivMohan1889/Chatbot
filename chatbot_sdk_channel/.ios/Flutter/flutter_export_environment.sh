8bc8d4724a21879d22ba87d5e722e98b2b416e9f
Shiv Mohan Singh <shiv@tribondinfosystems.com>
2024-03-08 12:18:50 +0530

    s

--- chatbot_sdk_channel/.ios/Flutter/flutter_export_environment.sh
+++ chatbot_sdk_channel/.ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/shivmohansingh/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/shivmohansingh/Desktop/ChatbotSdk/flutter_module"
+export "FLUTTER_APPLICATION_PATH=/Users/shivmohansingh/Desktop/Chatbot/chatbot_sdk_channel"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.14"
+export "FLUTTER_BUILD_NAME=1.0.15"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
